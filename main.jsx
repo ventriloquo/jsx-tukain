3b26c9d7dd0a1599a47530f61f93cc9839f168ed
Ventriloquo <dfern3321@gmail.com>
2025-05-13 21:35:22 -0300

    Atualizar o main.jsx

--- main.jsx
+++ main.jsx
@@ -23,307 +23,75 @@ console.log("Listening on http://localhost:8000");
 
 function App({ children }) {
   return (
-    <html>
+    <html data-theme="dark">
       <head>
         <link rel="icon" type="image/png" href="https://f4.bcbits.com/img/0038995394_41.jpg" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlightjs-themes@1.0.0/tomorrow-night-eighties.css"/>
-        <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"/>
         <style>
           {`
-            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
-
             :root {
-              --bg-0:     #1F1F28;
-              --bg-1:     #363646;
-              --bg-2:     #2A2A37;
-              --fg-1:     #DCD7BA;
-              --fg-2:     #717C7C;
-              --accent-1: #9CABCA;
-              --accent-2: #957FB8;
+              --bg-0: #1f1f28;
+              --bg-2: #2a2a37;
+              --bg-1: #363646;
+              --fg-1: #dcd7ba;
+              --fg-2: #717c7c;
+              --accent-1: #957fb8;
+              --accent-2: #9cabca;
               --accent-3: #98bb6c;
-              --accent-4: #6A9589;
 
-              --border: solid 1px var(--bg-1);
+              --pico-background-color: var(--bg-0);
+              --pico-color: var(--fg-1);
+              --pico-muted-color: var(--fg-2);
+              --pico-primary: var(--accent-1);
+              --pico-secondary: var(--accent-2);
+              --pico-primary-hover: var(--accent-3);
+              --pico-code-background-color: var(--bg-1);
+              --pico-card-sectioning-background-color: var(--bg-1);
+              --pico-card-background-color: var(--bg-2);
+              --pico-muted-border-color: var(--pico-muted-color);
+              --pico-code-color: var(--pico-muted-color);
+              --pico-table-border-color: var(--pico-muted-color);
+              --pico-h1-color: var(--pico-color);
+              --pico-h2-color: var(--pico-color);
+              --pico-h3-color: var(--pico-color);
             }
-
-            * {
-              padding: 0;
-              margin: 0;
-            }
-
             ::-webkit-scrollbar {
               display: none;
             }
-
-            html {
-              font-family: "Noto Sans", sans-serif;
-              background-color: var(--bg-0);
+            main {
+              max-width: 80ch !important;
             }
-
-            body {
-              color: var(--fg-1);
-            }
-
-            mark {
-              background-color: #e6c384;
-            }
-
             a {
-              color: var(--accent-1);
-              text-decoration: none;
+              text-decoration: underline;
+              color: var(--pico-secondary);
             }
-
-            a:hover {
-              opacity: 80%;
-            }
-
-            .center {
-              display: flex;
-              flex-wrap: wrap;
-              justify-content: center;
-            }
-
-            ul {
-              list-style: "· "
-            }
-
-            .content {
-              display: block;
-              margin: 0 auto;
-              padding: 1em;
-              position: relative;
-              max-width: 80ch;
-              min-height: 100vh;
-            }
-
-            header {
-              position: sticky;
-              top: 0;
-              z-index: 10;
-              background-color: var(--bg-0);
-              color: var(--fg-1);
-              justify-content: space-between;
-              margin: auto;
-            }
-
-            footer {
-              padding: .5em 0;
-              border-top: solid 1px var(--bg-1)
-            }
-
-            nav {
-              background-color: var(--bg-0);
-              border-bottom: solid 1px var(--bg-1);
-              height: 60px;
-              display: flex;
-              align-items: center;
-              margin-bottom: 2em;
-            }
-
-            .links-container {
-              height: 100%;
-              width: 100%;
-              display: flex;
-              flex-direction: row;
-              align-items: center;
-            }
-
             nav a {
-              height: 100%;
-              padding: 0 20px;
-              display: flex;
-              align-items: center;
-              text-decoration: none;
-              color: var(--accent-2);
+              color: var(--pico-primary);
             }
-
-            nav a:hover {
-              background-color: var(--accent-2);
-              color: var(--bg-0);
-            }
-
-            nav .home-link {
-              margin-right: auto;
-            }
-
-            #sidebar-active {
-              display: none;
-            }
-
-            nav img, nav hgroup, .open-sidebar-button, .close-sidebar-button {
-              display: none;
-            }
-
-            .post {
-              display: block;
-              margin: auto;
-              margin: 1em 0;
-            }
-
-            button, .alternative_button {
-              border-radius: 3px;
-              box-shadow: 0px 1px 2px 0px black;
-              font-size: small;
-              margin: 5px;
-              padding: 6px 20px;
-              background-color: var(--bg-1);
-              color: var(--fg-1);
-              transition: all 0.2s;
-            }
-
-            button:hover {
-              opacity: 100%;
-              background-color: var(--accent-2);
-              color: var(--bg-0);
-            }
-
-            button:active, .alternative_button {
-              box-shadow: inset 0px 1px 2px 0px black;
-              background-color: var(--bg-2);
-              color: var(--fg-1);
-            }
-
-            button:active, .alternative_button:hover {
-              opacity: 100%;
-            }
-
-            hr {
-              border: none;
-              border-bottom: var(--border);
-              margin: 1em 16em;
-            }
-
-            time {
-              color: var(--accent-1);
-              font-size: small;
-            }
-
-            hgroup {
-              margin-bottom: 1em;
-              overflow: hidden;
-            }
-
-            hgroup p {
-              color: var(--fg-2);
-              margin-top: -1em;
-            }
-
-            h1, h2, h3 {
-              font-weight: normal;
-            }
-
-            h1 {
-              color: var(--accent-3);
-              font-size: x-large;
-            }
-
-            h3 {
-              color: var(--accent-4);
-              font-size: x-large;
-            }
-
-            p {
-              padding-top: .7em;
-              padding-bottom: .7em;
-            }
-
-            pre {
-              padding: 1em;
-            }
-
-            blockquote {
-                background-color: var(--bg-1);
-                color: var(--fg-1);
-                border-left: solid 5px var(--accent-1);
-                padding: 10px 20px;
-                margin: 10px auto;
-                margin-bottom: 1em;
-            }
-
-            img, iframe {
-              max-width: 90%;
-              max-height: 400px;
+            img {
+              max-width: 400px;
               margin: 2px;
             }
-
-            table, th, td {
-                padding: 5px;
-                border-collapse: collapse;
-            }
-
-            table {
-                border: none;
-                overflow: scroll;
-                margin: 1em;
-                margin-left: auto;
-                margin-right: auto;
-                border: var(--border);
-            }
-
-            th, td {
-                padding-left: 1em;
-                padding-right: 1em;
-                margin: 0;
-            }
-
-            th {
-                font-weight: bold;
-                background-color: var(--accent-1);
-            }
-
-            tr {
-                background-color: var(--bg-0);
-            }
-
-            tr:nth-of-type(even) {
-                background-color: var(--fg-2);
-                color: var(--fg-1);
-            }
-
-            thead > tr > th {
-                background-color: var(--accent-2);
-                color: var(--bg-0);
-                font-weight: bold;
-                border-bottom: var(--border);
-            }
-
-            colgroup {
-                border: solid 2px var(--fg-2);
-            }
-
             img:hover {
-              border: solid 2px var(--accent-1);
+              border: solid 2px var(--pico-primary);
               transition: all 50ms;
             }
             article ul {
               margin: .5em 1em;
             }
-            article code, kbd {
-              padding: 3px 6px;
-              border-radius: 3px ;
-            }
-            pre:has(code), code {
-              background-color: var(--bg-1) !important;
-              margin: 10px 0;
-              border-radius: 3px;
-              overflow-x: scroll;
-            }
-            kbd {
-              background-color: var(--fg-1);
-              color: var(--bg-0);
-            }
-            tr:nth-of-type(2n) {
-              background-color: var(--bg-1);
-            }
-            article {
-              background-color: var(--bg-2);
-              padding: 1em;
-              border-radius: 3px;
-            }
             article img {
               display: block;
               margin: auto;
               max-width: 300px;
+            }
+            article header {
+              color: var(--pico-muted-color);
+            }
+            blockquote {
+              background-color: var(--pico-code-background-color);
+            }
+            pre {
+              margin: 1em auto;
             }
             .gallery {
               display: flex;
@@ -332,172 +100,28 @@ function App({ children }) {
             }
             .gallery img {
               object-fit: cover;
-              max-width: 300px;
             }
             .gallery.albun img {
               max-width: 200px;
               margin: 2px;
-            }
-                        @media only screen and (max-width: 768px) {
-              #search {
-                width: 40ch;
-              }
-
-              nav {
-                backdrop-filter: none;
-                background-color: var(--bg-0);
-                z-index: 100;
-              }
-
-              nav img {
-                display: block;
-                margin: 0.8em;
-                border: var(--border);
-                border-radius: var(--border-radius);
-                max-width: 30%;
-              }
-
-              .links-container {
-                flex-direction: column;
-                align-items: flex-start;
-
-                position: fixed;
-                top: 0;
-                left: -102%;
-                z-index: 100;
-                width: 100%;
-
-                background-color: var(--bg-2);
-                transition: 0.30s ease-out;
-              }
-
-              nav a {
-                box-sizing: border-box;
-                height: auto;
-                width: 100%;
-                padding: 20px 30px;
-                justify-content: flex-start;
-              }
-
-              .open-sidebar-button, .close-sidebar-button {
-                padding: 20px;
-                display: block;
-              }
-
-              #sidebar-active:checked ~ .links-container {
-                left: 0;
-              }
-              
-              hr {
-                display: none;
-              }
-              
-              article {
-                background-color: initial;
-                border-top: solid 1px var(--bg-2);
-                padding: 1em 0;
-              }
-              
-              pre:has(code), code {
-                background-color: var(--bg-2) !important;
-              }
-            }
-
-            @media only screen and (max-width: 800px) {
-
-              h1 {
-                font-size: larger;
-              }
-              h2, h3 {
-                font-size: large;
-              }
             }
           `}
         </style>
         <title>Tukain</title>
       </head>
       <body>
-        <main>
+        <main class="container">
           <NavBar />
-          <div class="content">
+          <div>
             {children}
           </div>
         </main>
-        <script>hljs.highlightAll();</script>
       </body>
     </html>
   );
 }
 
 const posts = [
-  {
-    title: "HighlightJS + Outras atualizações",
-    date: "13/05/2025",
-    content: `Ontem eu tirei um pouco do meu tempo para procurar um |syntax-highlighter|
-    para o site, e achei o [highlightjs](https://highlightjs.org/). Eu tenho a impressão
-    de que já usei ele no passado, mas é difícil dizer ao certo.
-
-    Enfim.
-
-    Agora os blocos de código do site tem *cores*!
-    <pre><code>console.log("AEEEEEEEE, KASINÃOOO!!!")</code></pre>
-    <center>[ASSISTA ESSE VÍDEO.](https://www.youtube.com/watch?v=LCDaw0QmQQc)</center>
-
-    Isso facilita bastante se levar em conta que um dos assuntos que eu mais gosto de comentar
-    sobre são assuntos que necessitam de blocos de código.
-
-    Outra coisa também é que o HighlightJS detecta _automagicamente_ qual a linguagem que
-    está no bloco de código, ou tenta pelo menos achar a mais próxima. Por exemplo, ele
-    não tem um highlighting para a [Hare](https://harelang.org), então o highlighting vai
-    derivar do highlighting para a |Zephir|, o |C++| e a |Rust|. Olha aqui:
-
-    <pre>C++, não sei dizer o porquê<code>
-    use fmt;
-
-    export fn main() void = {
-      fmt::println("Olá, mundo!")!;
-    };
-    </code>Zephir, por conta do for-loop que se comporta como while-loop<code>
-    use fmt;
-
-    export fn main() void = {
-      let saudacoes = [
-        "Salve",
-        "Dale",
-        "Opa"
-      ];
-
-      for (let saudacao .. saudacoes) {
-        fmt::printfln("{}, mundo", saudacao)!;
-      };
-    };
-    </code>Rust, bastando colocar um type-casting:<code>
-    use fmt;
-
-    export fn main() void = {
-      let saudacoes = [
-        "Salve",
-        "Dale",
-        "Opa"
-      ];
-
-      for (let saudacao .. saudacoes) {
-        fmt::printfln("{}, mundo", saudacao: str)!;
-      };
-    };
-    </code></pre>
-    Inclusive, fica aqui a minha recomendação, teste a Hare.
-
-    # Além disso...
-    Eu venho aprimorando cada vez mais a minha configuração do [Neovim](https://neovim.io/),
-    eu diria que eu está ficando próximo do que seria considerado uma
-    [IDE](https://pt.wikipedia.org/wiki/Ambiente_de_desenvolvimento_integrado) leve. E também eu coloquei
-    uma caralhada de temas nele, você só tem que apertar 'espaço' 't' e uma janelinha com a seleção de temas
-    aparece. Na realidade, todas os atalhos extras que adicionei são acessados (e podem ser visualizados)
-    apertando 'espaço'.
-
-    Acho que é isso o que eu tenho para falar por agora, até mais!`
-  },
   {
     title: "Sintaxe no estilo Markdown!",
     date: "11/05/2025",
@@ -658,9 +282,8 @@ function Blog() {
       </hgroup>
     {posts.map((post) => (
       <div class="post">
-      <hr />
       <article>
-          <time>{post.date}</time>
+          <header>{post.date}</header>
           <h1>{post.title}</h1>
           <p dangerouslySetInnerHTML={{__html: format(post.content)}}></p>
       </article>
@@ -743,13 +366,13 @@ function NavBar() {
   return (
     <header>
       <nav>
-        <input id="sidebar-active" type="checkbox" />
-        <label class="open-sidebar-button" for="sidebar-active">Menu</label>
-        <div class="links-container">
-          <a class="home-link" href="/">Home</a>
-          <a href="/blog">Blog</a>
-          <a href="/gallery">Galeria</a>
-        </div>
+        <ul>
+          <li><a href="/">Home</a></li>
+        </ul>
+        <ul>
+          <li><a style="color: var(--pico-secondary)" href="/blog">Blog</a></li>
+          <li><a style="color: var(--pico-secondary)" href="/gallery">Galeria</a></li>
+        </ul>
       </nav>
     </header>
   );
